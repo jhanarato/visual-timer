6a1b7fcaa0e8ec14a941af1aa26f79091274f18b
Ajahn Jhanarato <jhanarato@gmail.com>
2022-07-09 10:06:43 +0800

    Some improvements:
    * Dictionary for RGB colour tuples.
    * Some more identifier renaming
    * Created a new MultiplierSelector class. Currently empty.

--- meditation_timer.py
+++ meditation_timer.py
@@ -5,15 +5,25 @@ from pmk import PMK
 hardware = Hardware()
 pmk = PMK(hardware)
 
+# Shortcuts for various switch LED colours
+key_colours = {"red": (255, 0, 0),
+               "green": (0, 255, 0),
+               "blue": (0, 0, 255)}
+
 
 class Timer:
+    """
+    Once we know the number of minutes assigned per key and
+    the number of keys selected, multiply the two and track
+    when the total number of minutes has passed.
+    """
     def __init__(self):
         self.is_set = False
-        self.base_minutes = 0
+        self.minutes = 0
         self.multiplier = 0
 
     def start(self):
-        minutes = self.base_minutes * self.multiplier
+        minutes = self.minutes * self.multiplier
         print(f"Timer set: {minutes} minutes")
         self.is_set = True
 
@@ -22,10 +32,14 @@ class Timer:
         return self.is_set
 
 
-class BaseMinutesSelector:
+class MinutesSelector:
+    """
+    Assign a handler to a single key to choose the number
+    of minutes to be multiplied.
+    """
     def __init__(self, key, timer_to_set, minutes, colour):
         self._key = key
-        self._key.set_led(*colour)
+        self._key.set_led(*key_colours[colour])
 
         self._timer = timer_to_set
         self._minutes = minutes
@@ -35,15 +49,27 @@ class BaseMinutesSelector:
     def _add_handler(self):
         @pmk.on_press(self._key)
         def handler(unit_key):
-            self._timer.base_minutes = self._minutes
+            self._timer.minutes = self._minutes
             self._timer.start()
 
 
+class MultiplierSelector:
+    """
+    Setup keypress handlers to choose how many times to multiply
+    the base number of minutes in order to select the total time.
+    """
+    def __init__(self):
+        pass
+
+
+# There is a single timer
 timer = Timer()
 
-five_minute_selector = BaseMinutesSelector(pmk.keys[0], timer, 5, (255, 0, 0))
-ten_minute_selector = BaseMinutesSelector(pmk.keys[4], timer, 10, (0, 255, 0))
-fifteen_minute_selector = BaseMinutesSelector(pmk.keys[8], timer, 15, (0, 0, 255))
+# Allow a choice of 5, 10 or 15 minutes. Red, green and blue respectively.
+five_minute_selector = MinutesSelector(pmk.keys[0], timer, 5, "red")
+ten_minute_selector = MinutesSelector(pmk.keys[4], timer, 10, "green")
+fifteen_minute_selector = MinutesSelector(pmk.keys[8], timer, 15, "blue")
+
 
 
 # TODO User selects the number to multiply the units.
