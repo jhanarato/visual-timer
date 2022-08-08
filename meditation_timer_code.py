f332bfed516463ea42ad009b0201eecefd18ac32
Ajahn Jhanarato <jhanarato@gmail.com>
2022-08-08 08:16:00 +0800

    Created meditation_timer_code.py to run the application.

---
+++ meditation_timer_code.py
@@ -0,0 +1,14 @@
+from pmk.platform.keybow2040 import Keybow2040 as Hardware
+from pmk import PMK
+
+import meditation_timer
+
+print("In Code.py")
+
+hardware = Hardware()
+pmk = PMK(hardware)
+meditation_timer.Hardware.set_hardware(pmk)
+sequence = meditation_timer.MenuSequence()
+
+while True:
+    sequence.do()
