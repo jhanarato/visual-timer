58f9011a8c17c34202f8880b09df29b304385444
Ajahn Jhanarato <jhanarato@gmail.com>
2022-09-06 12:13:09 +0800

    Reverted TimerMonitor.show_indicator_view() as the new version didn't quite work.

--- meditation_timer.py
+++ meditation_timer.py
@@ -222,8 +222,14 @@ class TimerMonitor:
 
     def show_indicator_view(self):
         indicator_key = 0
-        set_all_keys_colour("none")
-        set_key_colour(indicator_key, "orange")
+        selected = {indicator_key}
+        not_selected = all_keys - selected
+
+        for key_num in selected:
+            set_key_colour(key_num, "orange")
+
+        for key_num in not_selected:
+            set_key_colour(key_num, "none")
 
     def monitor(self):
         while True:
