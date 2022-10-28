40b6dc325a2ed8c5826cdea296a773c54e8990c4
Ajahn Jhanarato <jhanarato@gmail.com>
2022-10-28 16:58:26 +0800

    Added more tests for events.

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,15 +1,23 @@
+import pytest
+
 from vtimer import events
 from vtimer.util import partial
 
 
-def test_subscribe():
-    def handler():
+@pytest.fixture
+def reset_subscriptions():
+    events.subscribers.clear()
+
+
+def test_subscribe(reset_subscriptions):
+    def handler(data):
         pass
 
     events.subscribe("test_event", handler)
+    events.post_event("test_event", None)
 
 
-def test_post_event():
+def test_post_event(reset_subscriptions):
     class Handler:
         def __init__(self):
             self.value = 0
@@ -25,13 +33,13 @@ def test_post_event():
     assert handler.value == 123
 
 
-def test_with_partial():
+def test_with_partial(reset_subscriptions):
     class ValueHolder:
         def __init__(self):
             self.value = 0
 
-    def handler(value_holder, value):
-        value_holder.value = value
+    def handler(holder, value):
+        holder.value = value
 
     value_holder = ValueHolder()
     value_holder.value = 50
@@ -44,6 +52,18 @@ def test_with_partial():
     assert value_holder.value == 100
 
 
-def test_keypress_logger():
+def test_subscribe_twice(reset_subscriptions):
+    def handler_one(data):
+        pass
+
+    def handler_two(data):
+        pass
+
+    events.subscribe("test_event", handler_one)
+    events.subscribe("test_event", handler_two)
+    events.post_event("test_event", None)
+
+
+def test_keypress_logger(reset_subscriptions):
 
     assert False
