d2531cf6e24446795620ae15d50025c781ad7080
Ajahn Jhanarato <jhanarato@gmail.com>
2022-10-29 17:52:03 +0800

    Introduced an Observer class

--- src/vtimer/events.py
+++ src/vtimer/events.py
@@ -1,4 +1,4 @@
-# This code is taken from Arjan Eggles' better python repository
+# This code is adapted from Arjan Eggles' better python repository
 # https://github.com/ArjanCodes/betterpython
 # Copyright 2021
 
@@ -16,3 +16,12 @@ def post_event(event_type: str, data):
         return
     for fn in subscribers[event_type]:
         fn(data)
+
+
+class Observer:
+    def __init__(self, event_type):
+        subscribe(event_type, self.receive)
+        self.has_seen = False
+
+    def receive(self, event):
+        self.has_seen = True
--- tests/test_waiting.py
+++ tests/test_waiting.py
@@ -2,6 +2,8 @@ import pytest
 
 import vtimer.menus
 import vtimer.waiting
+import vtimer.events
+
 from fake_keypad import FakeKeypad
 
 from test_events import reset_subscriptions
@@ -12,3 +14,9 @@ def test_wait_for_selection_ends_on_menu_event(reset_subscriptions):
     menu = vtimer.menus.Menu()
     menu.options = [vtimer.menus.MenuOption(0, "blue", 7)]
     vtimer.waiting.wait_for_selection(menu)
+
+
+def test_event_observer(reset_subscriptions):
+    observer = vtimer.events.Observer("test_event")
+    vtimer.events.post_event("test_event", 123)
+    assert observer.has_seen
