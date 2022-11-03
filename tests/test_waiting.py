7d33aa1e52783581cd7ea276fd931fc01158fbdf
Ajahn Jhanarato <jhanarato@gmail.com>
2022-11-03 16:44:21 +0800

    waiting.wait_for_selection() now works with our implemented FakeKeypad.

--- tests/test_waiting.py
+++ tests/test_waiting.py
@@ -1,15 +1,23 @@
-import vtimer.menus
-import vtimer.waiting
 import vtimer.events
+from vtimer.menus import Menu, MenuOption
+from vtimer.waiting import wait_for_selection
 
 from fake_keypad import FakeKeypad
 from test_events import reset_subscriptions
 
 
 def test_wait_for_selection_ends_on_menu_event(reset_subscriptions):
+    menu = Menu()
+
+    def press_handler(key):
+        menu.select(0)
+
     vtimer.keypad = FakeKeypad()
-    menu = vtimer.menus.Menu()
-    menu.options = [vtimer.menus.MenuOption(0, "blue", 7)]
+    vtimer.keypad.set_keypress_function(0, press_handler)
+    vtimer.keypad.keys[0].press()
+
+    menu.options = [MenuOption(0, "blue", 7)]
+
     vtimer.waiting.wait_for_selection(menu)
 
 
