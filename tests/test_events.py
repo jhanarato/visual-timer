40ded5f35db867ffffae9be1fa6ff7dbaf58d85c
Ajahn Jhanarato <jhanarato@gmail.com>
2022-11-08 12:27:17 +0800

    Added Handler class.

--- tests/test_events.py
+++ tests/test_events.py
@@ -82,3 +82,18 @@ def test_event_observer(reset_subscriptions):
     observer = vtimer.events.Observer("test_event")
     vtimer.events.post_event("test_event", 123)
     assert observer.has_seen
+
+
+class Handler:
+    def __init__(self):
+        self._event = None
+
+    def __call__(self, event):
+        self._event = event
+
+    @property
+    def event(self):
+        if self._event is None:
+            raise Exception("No event has arrived")
+
+        return self._event
